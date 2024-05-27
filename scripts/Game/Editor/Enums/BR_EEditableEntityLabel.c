d0dfacf162d2809a17a731316df9324b48bbce83
Zelik89 <58987775+Zelik89@users.noreply.github.com>
2024-05-27 13:55:36 -0400

    Added Game Mode Stuff

---
+++ scripts/Game/Editor/Enums/BR_EEditableEntityLabel.c
@@ -0,0 +1,4 @@
+modded enum EEditableEntityLabel
+{
+	FACTION_BR,
+};
---
+++ scripts/Game/GameMode/BR_PretGameModeStateComponent.c
@@ -0,0 +1,10 @@
+[ComponentEditorProps(category: "BattleRoyale/GameMode/Components", description: "Game Mode component responsible for adding a 'pre-game' period to the game mode.")]
+class BR_PreGameModeStateComponentClass : SCR_PreGameGameModeStateComponentClass
+{
+}
+
+//! Game mode component that handles the post-game period.
+class BR_PreGameModeStateComponent : SCR_PreGameGameModeStateComponent
+{
+
+}
